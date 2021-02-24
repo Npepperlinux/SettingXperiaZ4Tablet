911afeee09a78cbf5d127e76401d77c73f2b6941
Npepperlinux <19j31ryoga@jr-doc.shinshu-u.ac.jp>
2021-02-24 19:59:44 +0900

    'main'でコミット

--- docomo.sh
+++ docomo.sh
@@ -7,7 +7,9 @@ read -p "Do you install tools 'adb'? (y/n) :"
 adb shell pm list packages -f com.nttdocomo.android.applicationmanager
 adb pull /oem/priv-app/DcmAppManager_sha1/DcmAppManager_sha1.apk=com.nttdocomo.android.applicationmanager
 adb shell pm uninstall -k --user 0 com.nttdocomo.android.applicationmanager
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.areamail
 adb shell pm uninstall -k --user 0 com.nttdocomo.android.cloudset
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.dhome
 adb shell pm uninstall -k --user 0 com.nttdocomo.android.docomo_market
 adb shell pm uninstall -k --user 0 com.nttdocomo.android.idmanager
 adb shell pm uninstall -k --user 0 com.nttdocomo.android.initialization
@@ -16,9 +18,19 @@ adb shell pm uninstall -k --user 0 com.nttdocomo.android.iconcier
 adb shell pm uninstall -k --user 0 com.nttdocomo.android.mediaplayer
 adb shell pm uninstall -k --user 0 com.nttdocomo.android.remotelock
 adb shell pm uninstall -k --user 0 com.nttdocomo.android.schedulememo
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.store
 adb shell pm uninstall -k --user 0 com.nttdocomo.android.tapandpay
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.voiceeditor
 adb shell pm uninstall -k --user 0 com.nttdocomo.android.wipe
 adb shell pm uninstall -k --user 0 jp.co.nttdocomo.anshinmode
 adb shell pm uninstall -k --user 0 jp.co.nttdocomo.carriermail
 adb shell pm uninstall -k --user 0 jp.co.nttdocomo.lcsapp
 adb shell pm uninstall -k --user 0 jp.co.nttdocomo.saigaiban
+adb shell pm uninstall -k --user 0 com.sonymobile.androidapp.cameraaddon.areffect
+adb shell pm uninstall -k --user 0 com.sonymobile.moviecreator
+adb shell pm uninstall -k --user 0 com.sonymobile.moviecreator.rmm
+adb shell pm uninstall -k --user 0 com.sonyericsson.video
+adb shell pm uninstall -k --user 0 com.google.android.apps.books
+adb shell pm uninstall -k --user 0 com.google.android.googlequicksearchbox
+adb shell pm uninstall -k --user 0 com.google.android.play.games
+adb shell pm uninstall -k --user 0 com.mobisystems.fileman
